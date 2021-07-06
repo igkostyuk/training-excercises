b6222d26ea2f34ee7a8465c7acafdba07385b508
Igor kostyuk <ig.fire@gmail.com>
2021-07-06 15:25:29 +0300

    add day1 task1(2)

---
+++ day_1/homework/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func runTask(scanner *bufio.Scanner) (string, error) {
+	fmt.Print("Enter task number:")
+	scanner.Scan()
+	switch scanner.Text() {
+	case "1":
+		return Task1(scanner)
+	default:
+		return "", errors.New("task number must be in range 1-1")
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	result, err := runTask(scanner)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(result)
+}

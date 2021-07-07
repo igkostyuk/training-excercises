9e046646e63e0a64acc36dfd46e345e24102e144
Igor kostyuk <ig.fire@gmail.com>
2021-07-07 23:41:15 +0300

    add day2 task

---
+++ day_2/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	srv "github.com/igkostyuk/training-excercises/service"
+)
+
+// Data to decode
+var jsonStr = []byte(`
+{
+    "things": [
+        {
+            "name": "Alice",
+            "age": 37
+        },
+        {
+            "city": "Ipoh",
+            "country": "Malaysia"
+        },
+        {
+            "name": "Bob",
+            "age": 36
+        },
+        {
+            "city": "Northampton",
+            "country": "England"
+        },
+ 		{
+            "name": "Albert",
+            "age": 3
+        },
+		{
+            "city": "Dnipro",
+            "country": "Ukraine"
+        },
+		{
+            "name": "Roman",
+            "age": 32
+        },
+		{
+            "city": "New York City",
+            "country": "US"
+        }
+    ]
+}`)
+
+func main() {
+	// logger to Inject
+	logger := log.New(os.Stdout, "INFO: ", 0)
+	service := srv.New(logger)
+	service.Decode(jsonStr)
+}
